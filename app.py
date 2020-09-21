5deff9329f0ac9a22525ee6529b7e9b2fd723782
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-09-20 22:09:32 -0700

    updating index.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+from jinja2 import Environment, select_autoescape
 import scrape_mars
 
 # Create an instance of Flask
@@ -14,7 +15,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
     return render_template("index.html", mars=mars_data)
@@ -28,7 +29,7 @@ def scrape():
     mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect(url_for('home'))
